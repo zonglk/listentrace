5287a9071b08056b76c7167b74040e7ad83028d3
zonglk <604922726@qq.com>
2020-01-14 20:21:27 +0800

    Revert "5.0.0【缓存】"
    This reverts commit 55c4654da8094b104458de9b88e6da87a4409f9f.

---
+++ listentraceShare/LTShareTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  LTShareTableViewController.h
+//  listentraceShare
+//
+//  Created by 宗丽康 on 2020/1/14.
+//  Copyright © 2020 listentrace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LTShareTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ listentraceShare/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  listentraceShare
+//
+//  Created by luojie on 2020/1/13.
+//  Copyright © 2020 listentrace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
